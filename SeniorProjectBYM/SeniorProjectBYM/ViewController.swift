416a38d91af59135589c06e45b06cdbc6573ab47
maysrowlandjm <maysrowlandjm@mymail.vcu.edu>
2017-10-03 22:14:14 -0400

    iOS

---
+++ SeniorProjectBYM/SeniorProjectBYM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeniorProjectBYM
+//
+//  Created by Jordan Mays-Rowland on 10/3/17.
+//  Copyright © 2017 BYM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
