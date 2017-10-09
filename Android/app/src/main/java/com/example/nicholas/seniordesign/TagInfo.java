208e6fdd610020d3b8df90b0f63c215fe4c26327
bennettn <bennettna2@vcu.edu>
2017-10-08 21:10:29 -0400

    Added AltBeacon To Parse
    Added the altbeacon library to parse the bluetooth signal, now able to get accurate proximity UUID

--- Android/app/src/main/java/com/example/nicholas/seniordesign/TagInfo.java
+++ Android/app/src/main/java/com/example/nicholas/seniordesign/TagInfo.java
@@ -13,17 +13,26 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
 
+import java.util.Collection;
 import java.util.List;
 
-public class TagInfo extends AppCompatActivity {
+public class TagInfo extends AppCompatActivity implements BeaconConsumer {
 
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
@@ -31,12 +40,21 @@ public class TagInfo extends AppCompatActivity {
     private static final long SCAN_PERIOD = 10000;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
     TextView deviceInfo;
+    private BeaconManager beaconManager;
 
     @Override
     @TargetApi(23)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_info);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.bind(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser("CorrectParse").setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.setForegroundScanPeriod(5100);
+        beaconManager.setForegroundBetweenScanPeriod(2000);
+        beaconManager.setBackgroundScanPeriod(5100);
+        beaconManager.setBackgroundBetweenScanPeriod(2000);
+
         //Permissions
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // Android M Permission check 
@@ -61,13 +79,16 @@ public class TagInfo extends AppCompatActivity {
 
         final Button deviceInfoButton = (Button) findViewById(R.id.deviceInfoButton);
         deviceInfo = (TextView) findViewById(R.id.deviceInfoText);
+        deviceInfo.setFocusable(false);
+        deviceInfo.setFocusableInTouchMode(false);
+        deviceInfo.setClickable(false);
         deviceInfoButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                scanLeDevice(true);
-                deviceInfo.setFocusable(false);
+                //scanLeDevice(true);
+                /*deviceInfo.setFocusable(false);
                 deviceInfo.setFocusableInTouchMode(false);
-                deviceInfo.setClickable(false);
+                deviceInfo.setClickable(false);*/
             }
         });
 
@@ -80,9 +101,14 @@ public class TagInfo extends AppCompatActivity {
             super.onScanResult(callbackType, result);
             deviceCount++;
             BluetoothDevice curDevice = result.getDevice();
+            Beacon testBeacon;
+            //testBeacon =beaconManager.getBeaconParsers().get(0).fromScanData(result.getScanRecord().getBytes(),result.getRssi(),result.getDevice());
+            //deviceInfo.append(beaconManager.getBeaconParsers().get(0).fromScanData(result.getScanRecord().getBytes(),result.getRssi(),result.getDevice()).toString());
+            //if(testBeacon==null)deviceInfo.append("There is no hope");
+            //deviceInfo.append(testBeacon.getId1().toString());
             deviceInfo.append("\n"+"Device: " + Integer.toString(deviceCount)+ " ID: "+curDevice.getAddress()+  "\n");
             //deviceInfo.append(curDevice.toString());
-            deviceInfo.append(result.toString());
+            //deviceInfo.append(result.toString());
         }
 
         @Override
@@ -95,6 +121,30 @@ public class TagInfo extends AppCompatActivity {
             super.onScanFailed(errorCode);
         }
     };
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, org.altbeacon.beacon.Region region) {
+                try {
+                    if (beacons.size() > 0) {
+                        for (Beacon b : beacons) {
+                            deviceInfo.append(b.getId1().toString()+" \n"+"TimeDetected" + System.currentTimeMillis()+" \n");
+                        }
+                    }
+                } catch (Exception ex) {
+                    //Log.e(TAG_BEACON_ACTIVITY, "Error was thrown: " + ex.getMessage());
+                }
+            }
+        });
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            //Log.e(TAG_BEACON_ACTIVITY, "Error was thrown: " + e.getMessage());
+        }
+    }
 
     private void scanLeDevice(final boolean enable) {
 
