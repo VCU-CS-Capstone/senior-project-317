65ab1b9991e153b49b91ceb263e6116ff61f9507
Justin Yirka <yirkajk@users.noreply.github.com>
2017-12-12 19:36:21 -0500

    Added pruning for 'locations' node
    Set up database pruning for our dummy 'locations' node.
    Our old pruning method does not apply to 'locations'
    Set up a new pruning method just for 'locations', deleting anything older than 30min.

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -6,22 +6,46 @@ const functions = require('firebase-functions');
 // Max number of observations to save. ****ADJUST BEHAVIOR HERE****
 const MAX_LOG_COUNT = 5;
 
-// Removes siblings of the node that triggered the function if there are more than MAX_LOG_COUNT.
+// Oldest records to store for heatmap. Format: milliseconds
+const MAX_LOG_AGE = 1800000; // 30 minutes
+
 exports.truncate = functions.database.ref('/observations/{tagid}/{timestamp}').onWrite(event => {
 	// Use of {asdf} in file path represents wildcard.
 
 	const parentRef = event.data.ref.parent;
-	return parentRef.once('value').then(snapshot => {
-		if (snapshot.numChildren() >= MAX_LOG_COUNT) {
-			let childCount = 0;
+
+	const parentKey = parentRef.key;
+	
+	const currTime = new Date().getTime();
+	
+	if (parentKey == 'locations') {	// Pruning unique to the dummy "location" node.
+		return parentRef.once('value').then(snapshot => {
 			const updates = {};
 			snapshot.forEach(function(child) {
-				if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
+				if (child.key < currTime - MAX_LOG_AGE) {
 					updates[child.key] = null;
 				}
 			});
 		// Update the parent. This effectively removes the extra children.
 		return parentRef.update(updates);
-		}
-	});
+		});
+
+	} else {	// Normal pruing, for all other nodes.
+	// Removes siblings of the node that triggered the function if there are more than MAX_LOG_COUNT.
+		return parentRef.once('value').then(snapshot => {
+			if (snapshot.numChildren() >= MAX_LOG_COUNT) {
+				let childCount = 0;
+				const updates = {};
+				snapshot.forEach(function(child) {
+					if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
+						updates[child.key] = null;
+					}
+				});
+			// Update the parent. This effectively removes the extra children.
+			return parentRef.update(updates);
+			}
+		});
+
+	}
+		
 });
