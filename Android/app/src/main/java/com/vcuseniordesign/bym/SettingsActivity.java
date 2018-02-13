66f6febe91e39f5cd914faeaf477c3f828567197
Justin Yirka <yirkajk@users.noreply.github.com>
2018-02-13 14:16:01 -0500

    Integrated Firebase Auth with Android signin/signout
    Our old signin used some other google services API.
    Now, we can actually use our sign in's with Firebase.

--- Android/app/src/main/java/com/vcuseniordesign/bym/SettingsActivity.java
+++ Android/app/src/main/java/com/vcuseniordesign/bym/SettingsActivity.java
@@ -1,33 +1,17 @@
 package com.vcuseniordesign.bym;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SettingsActivity extends AppCompatActivity implements
-            GoogleApiClient.OnConnectionFailedListener,
             View.OnClickListener {
-
-    private static final String TAG = "SettingsActivity";
-    private static final int RC_SIGN_IN = 9001;
-
-    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +21,7 @@ public class SettingsActivity extends AppCompatActivity implements
         // Button listeners
         findViewById(R.id.sign_out_button).setOnClickListener(this);
         findViewById(R.id.go_back_button).setOnClickListener(this);
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
     }
-
-
 
     @Override
     public void onClick(View v) {
@@ -72,10 +41,10 @@ public class SettingsActivity extends AppCompatActivity implements
     }
 
     private void signOut() {
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
                         launchSignInActivity();
                     }
                 });
@@ -84,12 +53,5 @@ public class SettingsActivity extends AppCompatActivity implements
     private void launchSignInActivity() {
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-        // be available.
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 }
--- Android/app/src/main/java/com/vcuseniordesign/bym/SignInActivity.java
+++ Android/app/src/main/java/com/vcuseniordesign/bym/SignInActivity.java
@@ -1,24 +1,18 @@
 package com.vcuseniordesign.bym;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.altbeacon.beacon.Beacon;
 
@@ -26,58 +20,31 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * Code based on tutorial at https://developers.google.com/identity/sign-in/android/sign-in
- *
- * Presents initial screen to user to sign-in using a Google account.
- *
- * onStart() provides silent sign-in if user has stored credentials.
- *
- * onClick() responds to Google sign-in button. Calls signIn() method.
- * onActivityResult() responds to asynchronous activity which signIn() launched.
- * UI is updated by updateUI() based on result.
- *
- * Still need to implement sign-out on home screen, and deal with cases of no connection.
+ *  Sign-in implemented using this tutorial: https://firebase.google.com/docs/auth/android/firebaseui
  */
-public class SignInActivity extends AppCompatActivity implements
-        GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "SignInActivity"; //used in LOG calls
-    private static final int RC_SIGN_IN = 9001; //Code to send to google API for *sign-in* requests.
 
-    private GoogleApiClient mGoogleApiClient;
-    private TextView mStatusTextView;
-    private ProgressDialog mProgressDialog;
+    private static final int RC_SIGN_IN = 123; //can be anything
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_sign_in);
-
-
-        // Views
-        mStatusTextView = (TextView) findViewById(R.id.sign_in_status);
 
         // Button listeners
         findViewById(R.id.sign_in_button).setOnClickListener(this);
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
         ((BeaconApplication)getApplication()).setSavedBeacons(getSavedBeaconsFromFile());
         ((BeaconApplication)getApplication()).setFoundBeaconEvents(getFoundBeaconsFromFile());
 
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+        signIn();
     }
 
     private ArrayList<Beacon> getSavedBeaconsFromFile(){
@@ -117,36 +84,59 @@ public class SignInActivity extends AppCompatActivity implements
         return acquiredSavedBFE;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private void signIn() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build() );
 
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-        if (opr.isDone()) {
-            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-            // and the GoogleSignInResult will be available instantly.
-            Log.d(TAG, "Got cached sign-in");
-            GoogleSignInResult result = opr.get();
-            handleSignInResult(result);
-        } else {
-            // If the user has not previously signed in on this device or the sign-in has expired,
-            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-            // single sign-on will occur in this branch.
-            showProgressDialog();
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(GoogleSignInResult googleSignInResult) {
-                    hideProgressDialog();
-                    handleSignInResult(googleSignInResult);
-                }
-            });
-        }
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        hideProgressDialog();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                updateUI(true);
+            } else {
+                // Sign in failed, check response for error code
+                updateUI(false);
+            }
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.INVISIBLE);
+            ((TextView)findViewById(R.id.sign_in_status)).setText(R.string.signed_in);
+
+            Intent intent = new Intent(this, TagInfo.class);
+            startActivity(intent);
+
+        } else {
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+
+            TextView statusTextView = findViewById(R.id.sign_in_status);
+
+            if (isOffline()) {
+                statusTextView.setVisibility(View.VISIBLE);
+                statusTextView.setText(R.string.no_connection);
+            } else {
+                statusTextView.setVisibility(View.VISIBLE);
+                statusTextView.setText(R.string.not_signed_in);
+            }
+        }
     }
 
     @Override
@@ -158,76 +148,18 @@ public class SignInActivity extends AppCompatActivity implements
         }
     }
 
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+    /**
+     * See: https://github.com/firebase/FirebaseUI-Android/blob/master/auth/src/main/java/com/firebase/ui/auth/KickoffActivity.java
+     * Check if there is an active or soon-to-be-active network connection.
+     *
+     * @return true if there is no network connection, false otherwise.
+     */
+    private boolean isOffline() {
+        ConnectivityManager manager = (ConnectivityManager) getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            updateUI(true);
-        } else {
-            // Signed out, show unauthenticated UI.
-            updateUI(false);
-        }
-    }
-
-    private void updateUI(boolean signedIn) {
-        if (signedIn) {
-            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-            mStatusTextView.setText(R.string.signed_in);
-
-
-            Intent intent = new Intent(this, TagInfo.class);
-            startActivity(intent);
-
-        } else {
-            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-            mStatusTextView.setText(R.string.not_signed_in);
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-        // be available.
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mProgressDialog != null) {
-            mProgressDialog.dismiss();
-        }
-    }
-
-    private void showProgressDialog() {
-        if (mProgressDialog == null) {
-            mProgressDialog = new ProgressDialog(this);
-            mProgressDialog.setIndeterminate(true);
-        }
-
-        mProgressDialog.show();
-    }
-
-    private void hideProgressDialog() {
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            mProgressDialog.hide();
-        }
+        return !(manager != null
+                && manager.getActiveNetworkInfo() != null
+                && manager.getActiveNetworkInfo().isConnectedOrConnecting());
     }
 }
